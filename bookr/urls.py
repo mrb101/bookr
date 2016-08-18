f384372f00e61ff61b449a0a8cf4f85cadb22124
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-08-19 07:20:36 +0800

    Adds initial urls | not working.. I go sleep

--- bookr/urls.py
+++ bookr/urls.py
@@ -16,6 +16,30 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.contrib.auth import views as auth_views
+
+from books import views as books_views
+from discussion import views as topic_views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/', auth_views.login, name="login"),
+    url(r'^logout/', auth_views.logout, name="logout"),
+
+    url(r'^category/$', categories_views.CategoryList.as_view(), name="categories_list"),
+    url(r'^category/add/$', categories_views.CategoryAdd.as_view(), name="categorie_add"),
+    url(r'^category/update/$',categories_views.CategoryUpdate.as_view(), name="categorie_update"),
+    url(r'^cateogry/(?P<slug>[-\w]+)/$', categories_views.CategoryDetail.as_view(), name="categorie_detail"),
+
+    url(r'^books/$', books_views.BookList.as_view(), name="books_list"),
+    url(r'^books/add/$', books_views.BookAdd.as_view(), name="book_add"),
+    url(r'^books/update/$',books_views.BookUpdate.as_view(), name="book_update"),
+    url(r'^books/(?P<slug>[-\w]+)/report', books_views.BookReport.as_view(), name="book_report"),
+    url(r'^books/(?P<slug>[-\w]+)/$', books_views.BookDetail.as_view(), name="book_detail"),
+
+    url(r'^topics/$', topics_views.TopicsList.as_view(), name="topics_list"),
+    url(r'^topics/add/$', topics_views.TopicAdd.as_view(), name="topic_add"),
+    url(r'^topics/update/$',topics_views.TopicUpdate.as_view(), name="topic_update"),
+    url(r'^topics/(?P<slug>[-\w]+)/report', topics_views.TopicReport.as_view(), name="topic_report"),
+    url(r'^topics/(?P<slug>[-\w]+)/$', topics_views.TopicDetail.as_view(), name="topic_detail"),
 ]
