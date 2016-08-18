36bf4d3bfe9dfb7ce2aaf192c282f61e0a29e290
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-08-19 07:53:46 +0800

    fixes debug_toolbar typo

--- bookr/settings.py
+++ bookr/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third party apps
-    'django_toolbar',
+    'debug-toolbar',
     # my apps
     'books',
     'discussions'
