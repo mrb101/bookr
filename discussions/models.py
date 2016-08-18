1aa4107b33993179095ddedda1cb05e3a48f5942
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-08-19 07:59:47 +0800

    fixes typo in import

--- discussions/models.py
+++ discussions/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-from books.model import Book
+from books.models import Book
 
 from django.core.urlresolvers import reverse
 from django.utils.text import slugify
