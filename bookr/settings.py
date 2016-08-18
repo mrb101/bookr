54b98c624b610da7e3d46b2b13afdeb34e9b6d50
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-08-19 07:54:26 +0800

    fixes debug_toolbar again. i forgot how to type

--- bookr/settings.py
+++ bookr/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third party apps
-    'debug-toolbar',
+    'debug_toolbar',
     # my apps
     'books',
     'discussions'
