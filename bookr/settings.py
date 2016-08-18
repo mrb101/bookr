c17ee40c9f638838c6cfca0d14c5f5c852390e48
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-08-19 04:32:41 +0800

    Allocates static/media files

--- bookr/settings.py
+++ bookr/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'bookr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(PROJECT_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
+
 STATIC_URL = '/static/'
+MEDIA_URL = 'media'
+
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media_cdn')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static_cdn')
