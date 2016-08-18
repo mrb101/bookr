1ac97544bbcf94ca5a021513ec77cde7a7d6a7d7
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-08-19 07:51:19 +0800

    adds discussions intial views

--- discussions/views.py
+++ discussions/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+class TopicList(ListView):
+    model = Topic
+    tempalte_name = 'topics/list.html'
+    context_object_name = 'topics'
+
+
+class TopicDetail(DetailView):
+    model = Topic
+    template_name = 'topics/detail.html'
+    context_object_name= 'topic'
+
+
+class TopicCrate(CreateView):
+    pass
+
+
+class TopicUpdate(UpdateView):
+    pass
+
+
