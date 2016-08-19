cb10a6492b43e6267484b4f6f9414d7d1bcbdf6c
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-08-19 21:35:21 +0800

    adds books model to admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from imagekit.admin import AdminThumbnail
+
+from .models import Category, Author, Book
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'admin_thumbnail')
+    admin_thumbnail = AdminThumbnail(image_field='image_thumbnail')
+
+admin.site.register(Category)
+admin.site.register(Author)
+admin.site.register(Book, BookAdmin)
