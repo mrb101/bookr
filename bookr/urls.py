3d627c34b577f8d2f4781bfae67e3572665a13c4
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-08-19 21:34:42 +0800

    adds media files url

--- bookr/urls.py
+++ bookr/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 
 from django.contrib.auth import views as auth_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from books import views as books_views
 from discussions import views as topics_views
 
@@ -43,3 +46,9 @@ urlpatterns = [
     url(r'^topics/(?P<slug>[-\w]+)/report', topics_views.TopicReport.as_view(), name="topic_report"),
     url(r'^topics/(?P<slug>[-\w]+)/$', topics_views.TopicDetail.as_view(), name="topic_detail"),
 ]
+if settings.DEBUG is True:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+
