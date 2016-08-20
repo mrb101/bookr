2964fa68c67dd0ecbbc405d4b7571bb1613b2d84
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-08-20 16:29:55 +0800

    Adds discussions form

---
+++ discussions/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm
+
+from django.forms.widgets import FileInput
+from .models import Topic
+
+
+class TopicForm(ModelForm):
+    class Meta:
+        model = Topic
+        fields = ['title',
+                  'Description',
+                  'book',
+                  'user',
+                 ]
