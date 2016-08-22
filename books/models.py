6de3b83de942523145405323b314aea206cd7902
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-08-23 01:38:23 +0800

    Makes TimeStamp model abstract

--- books/models.py
+++ books/models.py
@@ -20,6 +20,10 @@ class TimeStampModel(models.Model):
     created = models.DateTimeField(auto_now=True, auto_now_add=False)
     updated = models.DateTimeField(auto_now=False, auto_now_add=True)
 
+    class Meta:
+        abstract = True
+
+
 
 class Publisher(TimeStampModel):
     pass
