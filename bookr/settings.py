6ca5da927bb2f2abb7621eec44c7f15339fe1824
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-08-23 01:36:35 +0800

    Remove debug_toolbar

--- bookr/settings.py
+++ bookr/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third party apps
-    'debug_toolbar',
     # my apps
     'books',
     'discussions'
